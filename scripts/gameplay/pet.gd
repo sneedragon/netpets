7e74dd3fdbefddf155ad7c5d23cbd1cfa00d3da1
Charles Sneedworth <sneedworth@sevensevenseven.net>
2025-03-09 22:23:14 +0100

    improved age scaling

--- scripts/gameplay/pet.gd
+++ scripts/gameplay/pet.gd
@@ -24,7 +24,10 @@ func display_pet():
 	sprite.scale.x = 4 * global.data["pet_attributes"]["width"]
 	sprite.scale.y = 4 * global.data["pet_attributes"]["height"]
 	sprite.modulate = Color.from_hsv(global.data["pet_attributes"]["hue"], 1.0, 1.0)
-	sprite.scale *= 1 * (((float(pet_manager.get_pet_level()) / 10) + 0.5))
+	var age_factor = 1 * clamp((1 * ((((float(pet_manager.get_pet_level()) / 25) + 0.5)))), 0.1, 1.2)
+	sprite.scale *= age_factor
+	sprite.scale.x = clamp(sprite.scale.x, 1, 10)
+	sprite.scale.y = clamp(sprite.scale.y, 1, 10)
 	print("SPRITE SCALE" + str(sprite.scale))
 	
 	
