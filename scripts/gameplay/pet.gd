e4363d6564546a0d91e5e04bbb1794871b0a7b07
Charles Sneedworth <sneedworth@sevensevenseven.net>
2025-03-09 22:02:05 +0100

    fixed numbers, added age scaling

--- scripts/gameplay/pet.gd
+++ scripts/gameplay/pet.gd
@@ -2,6 +2,9 @@ extends Node2D
 
 @onready var sprite = $PetSprite
 
+@onready var pet_manager: Node = $PetManager
+
+
 func _process(delta: float) -> void:
 	if !global.data["light"]:
 		sprite.animation = "Sleep"
@@ -12,8 +15,8 @@ func generate_pet():
 	global.data["pet_exists"] = true
 	global.data["pet_alive"] = true
 	global.data["pet_stats"]["birth_time"] = Time.get_unix_time_from_system()
-	global.data["pet_attributes"]["height"] = randf_range(0.75, 1)
-	global.data["pet_attributes"]["width"] = randf_range(1.5, 1.5)
+	global.data["pet_attributes"]["height"] = randf_range(0.75, 1.5)
+	global.data["pet_attributes"]["width"] = randf_range(0.75, 1.5)
 	global.data["pet_attributes"]["hue"] = randf_range(0.0, 1.0)
 	global.save_data()
 
@@ -21,4 +24,7 @@ func display_pet():
 	sprite.scale.x = 4 * global.data["pet_attributes"]["width"]
 	sprite.scale.y = 4 * global.data["pet_attributes"]["height"]
 	sprite.modulate = Color.from_hsv(global.data["pet_attributes"]["hue"], 1.0, 1.0)
+	sprite.scale *= 1 * (((float(pet_manager.get_pet_level()) / 10) + 0.5))
+	print("SPRITE SCALE" + str(sprite.scale))
+	
 	
